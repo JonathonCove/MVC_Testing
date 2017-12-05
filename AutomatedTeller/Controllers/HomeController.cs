94101403903437a8a722c0c2f1c8ee7e3cdad459
Jonathon <Jonathon@coalfacedevelopment.com>
2017-12-05 13:58:19 +0000

    First push of a basic project made by following https://www.youtube.com/watch?v=zWFoZb6EiwU&t=4762s

---
+++ AutomatedTeller/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using AutomatedTeller.CustomAttributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AutomatedTeller.Controllers
+{
+    [MyLoggingFilter]
+    public class HomeController : Controller
+    {
+        public ActionResult Index() {
+            return View();
+        }
+
+        public ActionResult About() {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact() {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        //the below method can be replaced with this one, but inclding both calls an abiguity exception
+        //[HttpPost]
+        //public ActionResult Contact(FormCollection theForm) {
+        //    ViewBag.EnteredMessage = theForm["message"] + " to you too";
+        //    PartialViewResult pvr = PartialView("ContactResponse");
+
+        //    //ViewBag.htmlMessage = pvr.
+        //    return PartialView("ContactResponse");
+        //    return View();
+        //}
+
+        [HttpPost]
+        public ActionResult Contact(string message) {
+            ViewBag.Message = message + " to you too";
+
+            return PartialView("ContactResponse");
+        }
+
+        [HttpGet]
+        public ActionResult GetContactRes(string message) {
+            ViewBag.Message = message + " to you too";
+
+            return PartialView("ContactResponse");
+        }
+    }
+}
---
+++ AutomatedTeller/Models/CheckingAccount.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AutomatedTeller.Models
+{
+    public class CheckingAccount
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [RegularExpression(@"\d{6,10}", ErrorMessage="Account Number must be between 6 and 10 characters")]
+        [Display(Name="Account No")]
+        public string AccountNumber { get; set; }
+
+        [Required]
+        [Display(Name="First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        public decimal Balance { get; set; }
+
+        [Display(Name="Full Name")]
+        public string FullName //needs to be on two lines for the display attribute
+            => FirstName + " " + LastName;
+    }
+}
