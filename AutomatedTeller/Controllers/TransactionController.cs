73aed7f67b1748518db024d9491b569eff714623
Jonathon <Jonathon@coalfacedevelopment.com>
2017-12-14 17:32:52 +0000

    Added more forms using ajax through MS and JS

---
+++ AutomatedTeller/Controllers/TransactionController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+
+namespace AutomatedTeller.Controllers
+{
+    public class TransactionController : Controller
+    {
+        // GET: Transaction
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Transaction/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Transaction/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Transaction/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+
+                decimal Amount = decimal.Parse(collection["Amount"]);
+
+                Models.TransactionModel tm = new Models.TransactionModel() {
+                    Amount = Amount,
+                    CheckingAccountId = int.Parse(User.Identity.GetUserId())
+                };
+
+                //db.TransactionModels.Add(tm);
+                //db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Transaction/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Transaction/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Transaction/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Transaction/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
