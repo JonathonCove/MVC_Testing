20d0547e396a853aadc803761a65162a11d6787c
Jonathon <Jonathon@coalfacedevelopment.com>
2017-12-22 10:02:11 +0000

    Renamed the project to Scarecrow and added style assets

---
+++ Constants/Constants.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Constants
+{
+    public class Constants
+    {
+        public static string ErrorLogPath {
+            get {
+                return ((ConstantsConfig)System.Configuration.ConfigurationManager.GetSection(
+                    "constantsConfigGroup/constantsConfig")).ErrorLogPath;
+            }
+        }
+
+        public static string WebsiteName {
+            get {
+                return ((ConstantsConfig)System.Configuration.ConfigurationManager.GetSection(
+                    "constantsConfigGroup/constantsConfig")).WebsiteName;
+            }
+        }
+    }
+}
---
+++ Constants/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Constants")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Constants")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c732445c-8480-4a90-8f79-3ea605b027f9")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
